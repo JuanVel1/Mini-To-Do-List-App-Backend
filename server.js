a1baed57173799c9a14c9a3d174e151ee6505cc3
Juan Velasquez <juan.1701812756@ucaldas.edu.co>
2025-01-01 13:20:31 -0500

    Updating files package.json & server.js  to deploy

--- server.js
+++ server.js
@@ -68,5 +68,5 @@ app.delete('/todos/:id', async (req, res) => {
     }
 });
 
-const PORT = 3001;
+const PORT =  process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
